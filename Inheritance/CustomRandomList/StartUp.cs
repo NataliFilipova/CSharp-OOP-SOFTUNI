a5728f1014faa312d3994f61543f04908fb7d244
Natali Filipova <56434467+NataliFilipova@users.noreply.github.com>
2022-02-23 10:43:18 +0200

    Inheritance
    dasda

---
+++ Inheritance/CustomRandomList/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("pesho");
+            list.Add("pesho1");
+            list.Add("pesho2");
+        }
+    }
+}
